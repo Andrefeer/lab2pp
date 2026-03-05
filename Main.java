1c5446f42f4cd83f7a2a713844b963aa2a1a59a3
Andrefeer <andreiferariu2005@gmail.com>
2026-03-05 14:00:00 +0200

    problema3

--- Main.java
+++ Main.java
@@ -1,47 +1,50 @@
 package org.example;
 import org.graalvm.polyglot.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 class Main {
-    private static String PythonToUpper(String token){/// in litere mari
+    private static String Pythonproblema() {
         Context polyglot = Context.newBuilder().allowAllAccess(true).build();
-        Value result = polyglot.eval("python", "'"+token+"'.upper()");///apel func python
-        String resultString = result.asString();
-        polyglot.close();
-
-        return resultString;
-    }
-
-    private static String SumCRC(String token){
-
-        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
-        String temp = token.substring(1, token.length()-1);
-        String formula = "sum(1*ord(ch)**4 + 2*ord(ch)**3 + 3*ord(ch)**2 + 4*ord(ch) + 5 for ch in '" + temp + "')";
+        String formula = """
+                a = int(input("Nr de aruncari:"))
+                b = int(input("Nr de pajuri:"))
+                a,b""";
         Value result = polyglot.eval("python", formula);
         String resultString = result.toString();
         polyglot.close();
-
         return resultString;
     }
 
+    private static void PythonBinomial(Integer n, Integer x) {
+        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
+        polyglot.getBindings("python").putMember("n", n);
+        polyglot.getBindings("python").putMember("x", x);
+        String formula = """
+                import math
+                p = 0.5
+                distributie = []
+                
+                for k in range(n + 1):
+                    #probabilitate binomiala
+                    prob = math.comb(n, k) * (p**k) * ((1-p)**(n-k))
+                    distributie.append(prob)
+                    print(f"P(X = {k}) = {prob}")
+                
+                # Suma probabilitatilor
+                prob_cel_mult_x = sum(distributie[:x + 1])
+                print(f"Probabilitatea de a obține cel mult {x} pajuri din {n} aruncări este: {prob_cel_mult_x}")
+                """;
+        Value result = polyglot.eval("python", formula);
+        polyglot.close();
+    }
+
+    //functia MAIN
     public static void main(String[] args) {
         Context polyglot = Context.create();
-        HashMap<String, List<String>> valori = new HashMap<String, List<String>>();///lista de stringuri
-        Value array = polyglot.eval("js", "[\"If\",\"we\",\"run\",\"the\",\"java\",\"command\"];");
-        for (int i = 0; i < array.getArraySize();i++){
-            String element = array.getArrayElement(i).asString();
-            String upper = PythonToUpper(element);
-            String crc = SumCRC(upper);
-            valori.computeIfAbsent(crc, k -> new ArrayList<>()).add(upper);
-            System.out.println(upper + " -> " + crc);
-        }
-        System.out.println("Rezultate grupate după CRC:");
-        for( String key : valori.keySet() ){
-            System.out.println( key + " -> " + valori.get(key));
-        }
+        String nr = Pythonproblema();
+        System.out.println(nr);
+        nr = nr.replaceAll("[(,)]", "");
+        String[] a = nr.split(" ");
+        PythonBinomial(Integer.parseInt(a[0]), Integer.parseInt(a[1]));
         polyglot.close();
     }
 }
